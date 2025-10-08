ab52d4027e84109f9e0e825de87d646f6e157ff2
MahmoudMohamedAli <mahmoudalielkout@gmail.com>
2025-10-08 15:20:48 +0200

    implement to_int and is_digit functions

---
+++ MyLib_is_digit.c
@@ -0,0 +1,4 @@
+int MyLib_isdigit(int c)
+{
+    return (c <= '9' && c >= '0');
+}
---
+++ MyLib_str_to_int.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int MyLib_atoi(const char *str)
+{
+    int i;
+    int sign;
+    int container;
+
+    i = 0;
+    sign = 1;
+    container = 0;
+    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign *= -1;
+        i++;
+    }
+    while (str[i])
+    {
+        if (!(MyLib_isdigit(str[i])))
+            return (container * sign);
+        else
+            container = container * 10 + (const char)str[i] - 48;
+        i++;
+    }
+    return (container * sign);
+}
+
+int main()
+{
+    int x = MyLib_atoi("-90");
+    printf("hi %d", x + 4);
+}
